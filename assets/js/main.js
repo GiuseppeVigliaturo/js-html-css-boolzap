8be64b386abf7b38965914ea88a7f2a062e479c5
giuviglia94 <vigliaturo94@gmail.com>
2019-11-11 20:06:11 +0100

    aggiunta js

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+
+  //agganciamo al click sull'icona di invio la funzione di callback
+  $(".sendmsg").click(function(){
+
+  //ci salviamo il valore dell'input inserito dall'utente
+  var messaggio = $(".message").val();
+
+  // facciamo una copia del div con classe "msgsent" che sta dentro
+  // un div con ID "template"
+  var elmentmsg = $("#template .msgsent").clone();
+
+  console.log(elmentmsg);
+
+  // modifica questa copia di "msgsent" aggiungendogli il testo del messaggio
+  elmentmsg.find(".testo").text(messaggio);
+
+  // appendiamo una copia con testo valorizzato del div "msgsent"
+  $("#contenitore").append(elmentmsg);
+
+  // ripuliamo il contenuto dell'input, per UX
+  $(".message").val(" ");
+
+  })
+
+});
